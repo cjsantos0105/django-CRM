2594852d8c6294fa9e9d50cc7f154317477de7ca
christjoesantos@gmail.com <cj.santos-ext@enstack.com>
2023-06-28 15:13:38 +0800

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,15 @@
+import mysql.connector as mysql
+
+database = mysql.connect(
+    host="localhost",
+    user="root",
+    passwd="password",
+)
+
+# prepare a cursor object
+cursorObj = database.cursor()
+
+# Create a databe
+cursorObj.execute("CREATE DATABASE IF NOT EXISTS drm")
+
+print("All Done!")
